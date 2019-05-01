b1a383bc959a9950a0630b1ec953717495199664
prithugoswami <prithugoswami524@gmail.com>
2019-05-01 19:36:27 +0530

    Update Search class and Main
    Did some reformating and Refactoring
    Search class will now accept spaces in the query String.

--- src/com/oocpro/tmdbdesktop/Main.java
+++ src/com/oocpro/tmdbdesktop/Main.java
@@ -2,18 +2,20 @@ package com.oocpro.tmdbdesktop;
 
 import com.oocpro.tmdbdesktop.tmdb.Movie;
 import com.oocpro.tmdbdesktop.tmdb.Search;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 
 public class Main{
-    public static void main (String [] args)throws IOException{
+    public static void main (String[] args)throws IOException{
         System.out.println("Enter a movie name to search ");
-        BufferedReader mname = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader mname = (new BufferedReader(new InputStreamReader
+                    (System.in)));
         String Mov_name = mname.readLine();
         Search mv = new Search(Mov_name);
-        Movie det = new Movie(Search.IDs.get(1));
+        Movie det = new Movie(mv.movieIds.get(1));
         System.out.println(det.title + " ("+det.year+")");
         System.out.println("Plotline: " + det.plotline);
         System.out.println("Genres: "   + det.genres);
